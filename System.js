d1d1e24c3e237c4a53e3cd245822ac2efc89716d
Algolbarth <62349521+Algolbarth@users.noreply.github.com>
2021-10-29 15:10:41 +0200

    Add files via upload

---
+++ System.js
@@ -0,0 +1,37 @@
+function initialiser () {
+    Affichage = "";
+}
+
+function actualiser () {
+    document.getElementById("interface").innerHTML = Affichage;
+}
+
+function fonction (nom,link) {
+    Affichage += "<a href='javascript:" + link + "'>" + nom + "</a>";
+}
+
+function lien (nom,link) {
+    Affichage += "<a href='" + link + "'>" + nom + "</a>";
+}
+
+function afficher (texte) {
+    Affichage += texte;
+}
+
+function saut (nombre) {
+    for (let n=0;n<nombre;n++) {
+        Affichage += "<br/>";
+    }
+}
+
+function div (nom) {
+    Affichage += "<div id='" + nom + "'>";
+}
+
+function div_fin () {
+    Affichage += "</div>";
+}
+
+function actualiser_id (id,value) {
+    document.getElementById(id).innerHTML = value;
+}
