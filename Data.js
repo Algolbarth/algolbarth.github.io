702d3372742844f53069ea453a19ea91d54dfca2
Algolbarth <62349521+Algolbarth@users.noreply.github.com>
2022-01-21 15:14:51 +0100

    Update Data.js

--- Data.js
+++ Data.js
@@ -54,5 +54,5 @@ function ajouter_sites () {
     ajouter_site("Summoners War Wiki","https://summonerswarskyarena.info/");
 
     ajouter_site("JsRPG","JsRPG/",true);
-    ajouter_site("Dimension the Game","Dimension/index.html",true);
+    ajouter_site("Dimension the Game","Dimension/",true);
 }
