d0b3fa3ac64adaa2fb2df735be395c009b49d22f
Algolbarth <62349521+Algolbarth@users.noreply.github.com>
2022-05-27 10:56:24 +0200

    Add files via upload

---
+++ Data.js
@@ -0,0 +1,112 @@
+function ajouter_sites () {
+    ajouter_site("Google","https://www.google.fr/",[9]);
+    ajouter_site("Ecosia","https://www.ecosia.org/",[9]);
+    ajouter_site("Bing","https://www.bing.com/",[9]);
+    ajouter_site("Yahoo","https://yahoo.com",[9]);
+    ajouter_site("Youtube","https://www.youtube.fr/",[2,8]);
+    ajouter_site("Google Traduction","https://translate.google.fr",[6]);
+    ajouter_site("Gmail","https://mail.google.com");
+    ajouter_site("Google Drive","https://drive.google.com");
+    ajouter_site("Twitch","https://www.twitch.tv/",[2,8]);
+    ajouter_site("Facebook","https://www.facebook.com/",[2]);
+    ajouter_site("Messenger","https://www.facebook.com/messages",[2]);
+    ajouter_site("Twitter","https://twitter.com/",[2]);
+    ajouter_site("Instagram","https://www.instagram.com/",[2]);
+    ajouter_site("Pinterest","https://www.pinterest.fr/",[2]);
+    ajouter_site("Netflix","https://www.netflix.com/fr/",[8]);
+    ajouter_site("Amazon","https://www.amazon.fr/",[4]);
+    ajouter_site("Ebay","https://www.ebay.fr/",[4]);
+    ajouter_site("AliExpress","https://fr.aliexpress.com/",[4]);
+    ajouter_site("Wish","https://www.wish.com/",[4]);
+    ajouter_site("Wikipédia","https://fr.wikipedia.org/",[6]);
+    ajouter_site("Github","https://github.com/",[7]);
+    ajouter_site("Discord","https://discord.com/",[2]);
+    ajouter_site("Discord <i>Compte personnel</i>","https://discord.com/channels/@me",[2]);
+    ajouter_site("Guilded","https://www.guilded.gg/",[2]);
+    ajouter_site("Reddit","https://www.reddit.com/",[2]);
+    ajouter_site("4chan","https://www.4chan.org/",[2]);
+    ajouter_site("Google Chrome","https://www.google.com/intl/fr/chrome/",[7]);
+    ajouter_site("Mozilla Firefox","https://www.mozilla.org/",[7]);
+    ajouter_site("Kahoot","https://kahoot.it",[6]);
+    ajouter_site("WolframAlpha","https://www.wolframalpha.com/",[6]);
+    ajouter_site("OpenClassRoom","https://openclassrooms.com/fr/courses",[7]);
+    ajouter_site("Emurom","https://emurom.net",[3]);
+    ajouter_site("Pokepédia","https://www.pokepedia.fr/",[3,11]);
+    ajouter_site("Smogon University","https://www.smogon.com/",[3,11]);
+    ajouter_site("Pokestrat","https://pokestrat.io/",[3,11]);
+    ajouter_site("Pokemon Showdown <i>Site officiel</i>","https://pokemonshowdown.com",[3,11]);
+    ajouter_site("Pokemon Showdown <i>Jouer</i>","https://play.pokemonshowdown.com",[3,11]);
+    ajouter_site("Riot Games","https://www.riotgames.com/fr",[3]);
+    ajouter_site("League of Legends","https://na.leagueoflegends.com/fr-fr/",[3,12]);
+    ajouter_site("OP.GG","https://euw.op.gg/",[3,12]);
+    ajouter_site("OP.GG <i>champions</i>","https://www.op.gg/champion/statistics",[3,12]);
+    ajouter_site("Porofessor","https://porofessor.gg/fr",[3,12]);
+    ajouter_site("Minecraft Wiki","https://minecraft.fandom.com/fr/wiki/Minecraft_Wiki",[3]);
+    ajouter_site("Terraria Wiki","https://terraria.fandom.com/fr/wiki/Terraria_Wiki",[3]);
+    ajouter_site("Dragon Quest Fan","https://www.dragonquest-fan.com/",[3]);
+    ajouter_site("L'encyclopédie Moga","https://mogapedia.fandom.com/fr/wiki/Monster_Hunter_Wiki/",[3]);
+    ajouter_site("Kiranico Monster Hunter X","http://mhgen.kiranico.com/",[3]);
+    ajouter_site("Shadowverse Portal","https://shadowverse-portal.com/cards?format=0&lang=fr",[3]);
+    ajouter_site("Bandori Party","https://bandori.party/",[3]);
+    ajouter_site("Wowhead","https://fr.wowhead.com/",[3]);
+    ajouter_site("Where in Warcraft ?","https://www.kruithne.net/where-in-warcraft/",[3]);
+    ajouter_site("Summoners War : Sky Arena","https://sw.com2us.com/fr/skyarena",[3,10]);
+    ajouter_site("Summoners War Wiki","https://summonerswarskyarena.info/",[3,10]);
+    ajouter_site("Benetton","https://fr.benetton.com/",[5]);
+    ajouter_site("Pimkie","https://www.pimkie.fr/",[5]);
+    ajouter_site("Camaieu","https://www.camaieu.fr/",[5]);
+    ajouter_site("Cache Cache","https://www.cache-cache.fr/",[5]);
+    ajouter_site("DeFacto <i>France</i>","https://ma.defactofashion.com/",[5]);
+    ajouter_site("DeFacto <i>Turquie</i>","https://www.defacto.com.tr/kadin/",[5]);
+    ajouter_site("Youtube convertisseur en MP3","https://mp3y.download/fr/faster-mp3-convert",[8]);
+    ajouter_site("Dofus","https://www.dofus.com/fr/prehome",[3]);
+    ajouter_site("Wix","https://fr.wix.com/",[7]);
+    ajouter_site("Reverso <i>traducteur</i>","https://www.reverso.net/traduction-texte",[6]);
+    ajouter_site("Reverso <i>correcteur</i>","https://www.reverso.net/orthographe/correcteur-francais/",[6]);
+    ajouter_site("Sutom","https://sutom.nocle.fr/",[6]);
+    ajouter_site("Wordle","https://www.powerlanguage.co.uk/wordle/",[6]);
+    ajouter_site("Vinted","https://www.vinted.fr/",[5]);
+    ajouter_site("Leboncoin","https://www.leboncoin.fr/",[4]);
+    ajouter_site("Qwant","https://www.qwant.com/?l=fr",[9]);
+    ajouter_site("SWAFARM","https://swarfarm.com/",[3,10]);
+    ajouter_site("Summoners War Optimizer","https://tool.swop.one/",[3,10]);
+    ajouter_site("Mots Avec","https://motsavec.fr/",[6]);
+    ajouter_site("Chessle","https://jackli.gg/chessle/",[6]);
+    ajouter_site("Sedecordle","https://sedecordle.com/",[6]);
+    ajouter_site("Dordle","https://zaratustra.itch.io/dordle",[6]);
+    ajouter_site("Devdocs","https://devdocs.io/",[7]);
+    ajouter_site("Devdocs <i>hors-ligne</i>","https://devdocs.io/offline",[7]);
+    ajouter_site("Stack overflow","https://stackoverflow.com/",[7]);
+    ajouter_site("Heartstone","https://playhearthstone.com/fr-fr",[3]);
+    ajouter_site("Summoners War : Lost Centuria","https://summonerswar.com/fr/lostcenturia",[3,10]);
+    ajouter_site("Steam","https://store.steampowered.com/?l=french",[3,4]);
+    ajouter_site("Canvas","https://canvas.instructure.com/login/canvas");
+    ajouter_site("JeuxVidéos.com","https://www.jeuxvideo.com/",[2,3]);
+    ajouter_site("Minecraft <i>Site officiel</i>","https://www.minecraft.net/fr-fr",[3]);
+    ajouter_site("Minecraft.fr","https://minecraft.fr/",[3]);
+    ajouter_site("Orsay","https://www.orsay.com/de-de/",[5]);
+    ajouter_site("H&M","https://www2.hm.com/fr_fr/index.html",[5]);
+    ajouter_site("Kiabi","https://www.kiabi.com/",[5]);
+    ajouter_site("Gems of War Database","https://gowdb.com/fr-FR/",[3]);
+    ajouter_site("Doctissimo","https://www.doctissimo.fr/",[6]);
+
+    ajouter_site("Site","Site/index.html",[0]);
+    ajouter_site("JsRPG","JsRPG/index.html",[0]);
+    ajouter_site("JsTA","JsTA/index.html",[0]);
+    ajouter_site("Dimension","Dimension/index.html",[0]);
+    ajouter_site("Rogue","Rogue/index.html",[0]);
+
+    ajouter_dossier("Local");//0
+    ajouter_dossier("Favoris");//1
+    ajouter_dossier("Réseaux sociaux");//2
+    ajouter_dossier("Jeux vidéos");//3
+    ajouter_dossier("Achats");//4
+    ajouter_dossier("Vêtements");//5
+    ajouter_dossier("Culture");//6
+    ajouter_dossier("Informatique");//7
+    ajouter_dossier("Vidéos");//8
+    ajouter_dossier("Recherche");//9
+    ajouter_dossier("SW",[3]);//10
+    ajouter_dossier("Pokemon",[3]);//11
+    ajouter_dossier("League of Legends",[3]);//12
+}
