3027ced2ff802b5fd69f9511aa232f7e202cb268
Algolbarth <62349521+Algolbarth@users.noreply.github.com>
2022-09-29 00:00:37 +0200

    Update Jeu.js

--- Projet/Jeu.js
+++ Projet/Jeu.js
@@ -8,6 +8,8 @@ function menu_afficher() {
     initialiser();
     fonction("Retour", "jeu_afficher()");
     saut(2);
+    afficher(Jeu.compte.nom);
+    saut(2);
     fonction("Sauvegarder", "compte_sauvegarde()");
     saut(2);
     fonction("Déconnexion", "main_afficher()");
